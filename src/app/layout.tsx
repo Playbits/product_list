8fe7dc26996d4e9371a7ca8d9a603f5ab79a0289
Abiola Adele <abiola.adele@andela.com>
2024-08-24 14:28:07 +0100

    Bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Product Listing app",
-  description: "Switchie assessment ",
+  description: "Switchive assessment ",
 };
 
 export default function RootLayout({
